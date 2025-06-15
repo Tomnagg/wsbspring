6ec6411f893b7253b57f1226c3319bd1bfd16cfc
Tomnagg <naglaktomasz@gmail.com>
2025-06-16 01:07:04 +0200

    Delete CapWSB-FitnessTracker directory

--- CapWSB-FitnessTracker/src/main/java/com/capgemini/wsb/fitnesstracker/statistics/api/StatisticsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.wsb.fitnesstracker.statistics.api;
-
-public class StatisticsNotFoundException extends RuntimeException {
-    public StatisticsNotFoundException(String message) {
-        super(message);
-    }
-}
